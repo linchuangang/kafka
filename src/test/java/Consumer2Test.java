bba63698e3190727045e9c8441e29a34ef6163a5
lcg <chuangang.lin@inrevo.io>
2019-12-19 14:54:18 +0800

    update

--- src/test/java/Consumer2Test.java
+++ src/test/java/Consumer2Test.java
@@ -1,3 +1,5 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -5,8 +7,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Created by Administrator on 2018/7/11.
  */
 public class Consumer2Test {
+
+    private static final Logger logger = LoggerFactory.getLogger(Consumer2Test.class);
     //不同group下消费
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("consumer2.xml");
+//        System.out.println(System.getProperty("user.dir"));
+      logger.info("----------");
     }
 }
--- src/test/java/ProduceTest.java
+++ src/test/java/ProduceTest.java
@@ -26,7 +26,7 @@ public class ProduceTest extends AbstractTest {
         String value = simpleDateFormat.format(new Date());
         String ifPartition = "1";
         Integer partitionNum = 1;
-        String role = "test";//用来生成key
+        String role = "order";//用来生成key
         for (int i = 0; i <5; i++) {
             Map<String, Object> res = produceServer.sendMesForTemplate(topic,value+"_"+i, ifPartition, partitionNum, role);
             logger.info("测试结果如下：===============");
@@ -40,6 +40,23 @@ public class ProduceTest extends AbstractTest {
     @Test
     public void testOther() {
         String topic = "otherTopic";
+        String value = "other";
+        String ifPartition = "1";
+        Integer partitionNum = 1;
+        String role = "test";//用来生成key
+        for (int i = 0; i <5; i++) {
+            Map<String, Object> res = produceServer.sendMesForTemplate(topic,value+i, ifPartition, partitionNum, role);
+            logger.info("测试结果如下：===============");
+            String message = (String) res.get("message");
+            String code = (String) res.get("code");
+            logger.info("code:" + code);
+            logger.info("message:" + message);
+        }
+    }
+
+    @Test
+    public void testTest() {
+        String topic = "testTopic";
         String value = "test";
         String ifPartition = "1";
         Integer partitionNum = 1;
